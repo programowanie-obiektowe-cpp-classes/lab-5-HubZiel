58a5f89a8824e77cbb899e0a7968550f78a9737c
HubZiel <hub.zielinski02@gmail.com>
2025-11-28 04:30:28 +0100

    zaliczenie

--- foo.hpp
+++ foo.hpp
@@ -7,6 +7,29 @@
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    // Twoja implementacja tutaj
-    return {};
+  
+    for (Human& person : people) {
+        person.birthday();
+    }
+
+    size_t size = people.size();
+    std::vector< char > odpowiedz(size);
+
+    size_t i = 0;
+ 
+    for (auto it = people.rbegin(); it != people.rend(); ++it) {
+
+        bool lubi_jedno = !it->isMonster();
+
+        if (lubi_jedno) {
+            odpowiedz[i] = 'y';
+        }
+        else {
+            odpowiedz[i] = 'n';
+        }
+
+        i++;
+    }
+
+    return odpowiedz;
 }
